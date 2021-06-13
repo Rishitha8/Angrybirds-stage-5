c5240bc02441960fbe9993281776b639959a6fbb
Rishitha8 <82807753+Rishitha8@users.noreply.github.com>
2021-06-13 14:50:01 +0530

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -2,13 +2,16 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+  // this is for the visibility of pig.
     this.Visibility = 255;
   }
  display(){
+  // here we can check the speed of the pig in console. 
    console.log(this.body.speed);
    if(this.body.speed < 3){
      super.display();
    }
+  // here the pig will disappear when it's speed is more than 3.
    else{
     World.remove(world,this.body);
     push();
